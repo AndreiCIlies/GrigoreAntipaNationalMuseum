a92862d5cd083febe3cf9bbd7bec8ded76b5d23b
AndreiCIlies <116495791+AndreiCIlies@users.noreply.github.com>
2024-04-21 11:08:47 +0300

    added the third room

--- Museum_S3D/Museum_S3D/Scene.cpp
+++ Museum_S3D/Museum_S3D/Scene.cpp
@@ -75,7 +75,13 @@ void Scene::Render()
 	renderer->Draw(ResourceManager::GetTexture("wallTexture"), pCamera, glm::vec3(50.5f + resize, 4.5f + resize, 0.f), glm::vec3(), glm::radians(90.f), glm::vec3(0.f, 0.f, 1.f), NULL);
 	renderer->Draw(ResourceManager::GetTexture("ceilingTexture"), pCamera, glm::vec3(50.f, 5.f + resize, 0.f), glm::vec3(), 0.f, glm::vec3(0.f, 0.f, 1.f), NULL);
 
-	
+	// THIRD ROOM
+	renderer->Draw(ResourceManager::GetTexture("floorTexture"), pCamera, glm::vec3(100.0f, 0.f, 0.f), glm::vec3(), 0.f, glm::vec3(), NULL);
+
+	renderer->Draw(ResourceManager::GetTexture("wallTexture"), pCamera, glm::vec3(100.f, 4.5f + resize, -4.5f - resize), glm::vec3(), glm::radians(90.f), glm::vec3(1.f, 0.f, 0.f), NULL);
+	renderer->Draw(ResourceManager::GetTexture("wallTexture"), pCamera, glm::vec3(100.f, 4.5f + resize, 5.5f + resize), glm::vec3(), glm::radians(90.f), glm::vec3(1.f, 0.f, 0.f), NULL);
+	renderer->Draw(ResourceManager::GetTexture("wallTexture"), pCamera, glm::vec3(100.5f + resize, 4.5f + resize, 0.f), glm::vec3(), glm::radians(90.f), glm::vec3(0.f, 0.f, 1.f), NULL);
+	renderer->Draw(ResourceManager::GetTexture("ceilingTexture"), pCamera, glm::vec3(100.f, 5.f + resize, 0.f), glm::vec3(), 0.f, glm::vec3(0.f, 0.f, 1.f), NULL);
 }
 
 void Scene::RenderModels()
