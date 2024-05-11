0b894aaa5ce265782248d9c7bb3b5b9d848e6026
cristinalacovei <125617041+cristinalacovei@users.noreply.github.com>
2024-05-11 19:55:58 +0300

    resized models, added models in the second room

--- Museum_S3D/Museum_S3D/Scene.cpp
+++ Museum_S3D/Museum_S3D/Scene.cpp
@@ -1,4 +1,4 @@
-#include "Scene.h"
+﻿#include "Scene.h"
 #include "Camera.h"
 #include "ResourceManager.h"
 #include "Renderer.h"
@@ -33,7 +33,10 @@ void Scene::Init()
 	ResourceManager::LoadTexture("../../Textures/floor.jpg", "floorTexture");
 	ResourceManager::LoadTexture("../../Textures/wall.jpg", "wallTexture");
 	ResourceManager::LoadTexture("../../Textures/Ceiling.png", "ceilingTexture");
-	//ResourceManager::LoadTexture("../../Textures/forest.jpg", "forestTexture");
+	ResourceManager::LoadTexture("../../Textures/grassTexture.jpg", "grassTexture");
+	ResourceManager::LoadTexture("../../Textures/muzeu.jpg", "museumTexture");
+	ResourceManager::LoadTexture("../../Textures/door.jpg", "doorTexture");
+
 	
 
 	models = new map<std::string, Model>();
@@ -47,6 +50,9 @@ void Scene::Init()
 	models->insert({ "Wire", Model("../../3DObjects/Wire/wire.obj") });
 	models->insert({ "Dilophosaurus", Model("../../3DObjects/Dilophosaurus/dilophosaurus.obj") });
 	models->insert({ "Barosaurus", Model("../../3DObjects/Barosaurus/21534_Barosaurus_v1.obj") });
+	models->insert({ "Grass", Model("../../3DObjects/Grass/Grass.obj") });
+	models->insert({ "Bear", Model("../../3DObjects/Bear/Bear.obj") });
+
 
 	//Renderers
 	renderer = new Renderer(ResourceManager::GetShader("shaderFloor"));
@@ -67,20 +73,17 @@ void Scene::Render()
 	renderer->Draw(ResourceManager::GetTexture("wallTexture"), pCamera, glm::vec3(-50.f, 4.5f + resize, 5.5f + resize), glm::vec3(), glm::radians(90.f), glm::vec3(1.f, 0.f, 0.f), NULL);
 	renderer->Draw(ResourceManager::GetTexture("wallTexture"), pCamera, glm::vec3(-95.5f + resize, 4.5f + resize, 0.f), glm::vec3(), glm::radians(90.f), glm::vec3(0.f, 0.f, 1.f), NULL);
 	renderer->Draw(ResourceManager::GetTexture("ceilingTexture"), pCamera, glm::vec3(-50.f, 30.f + resize, 0.f), glm::vec3(), 0.f, glm::vec3(0.f, 0.f, 1.f), NULL);
-
 	// SECOND ROOM
 	renderer->Draw(ResourceManager::GetTexture("floorTexture"), pCamera, glm::vec3(), glm::vec3(), 0.f, glm::vec3(), NULL);
-
 	renderer->Draw(ResourceManager::GetTexture("wallTexture"), pCamera, glm::vec3(0.f, 4.5f + resize, -4.5f - resize), glm::vec3(), glm::radians(90.f), glm::vec3(1.f, 0.f, 0.f), NULL);
 	renderer->Draw(ResourceManager::GetTexture("wallTexture"), pCamera, glm::vec3(0.f, 4.5f + resize, 5.5f + resize), glm::vec3(), glm::radians(90.f), glm::vec3(1.f, 0.f, 0.f), NULL);
 	renderer->Draw(ResourceManager::GetTexture("wallTexture"), pCamera, glm::vec3(4.5f + resize, 4.5f + resize, 0.f), glm::vec3(), glm::radians(90.f), glm::vec3(0.f, 0.f, 1.f), NULL);
 	renderer->Draw(ResourceManager::GetTexture("wallTexture"), pCamera, glm::vec3(-5.5f - resize, 4.5f + resize, 0.f), glm::vec3(), glm::radians(90.f), glm::vec3(0.f, 0.f, 1.f), NULL);
 	renderer->Draw(ResourceManager::GetTexture("ceilingTexture"), pCamera, glm::vec3(0.f, 30.f + resize, 0.f), glm::vec3(), 0.f, glm::vec3(0.f, 0.f, 1.f), NULL);
+	renderer->Draw(ResourceManager::GetTexture("museumTexture"), pCamera, glm::vec3(0.f , 13.5f+resize, -4.4f - resize), glm::vec3(0.6f, 0.6f, 0.6f), glm::radians(270.f), glm::vec3(1.f, 0.f, 0.f), NULL);
+	renderer->Draw(ResourceManager::GetTexture("doorTexture"), pCamera, glm::vec3(4.4f + resize, 10.f, 0.f), glm::vec3(0.4f, 0.4f, 0.4f), glm::radians(90.f), glm::vec3(0.f, 0.f, 1.f), 'z');
+	renderPodium->Draw(ResourceManager::GetTexture("grassTexture"), pCamera, glm::vec3(0.f, 0.f, -20.f), glm::vec3(23.5f, 1.f, 10.f), 0.0f, glm::vec3(), NULL);
 	
-	// Forest WALL + PODIUM
-	// renderer->Draw(ResourceManager::GetTexture("forestTexture"), pCamera, glm::vec3(0.f - resize, 1.f, 0.f), glm::vec3(0.6f, 0.6f, 0.6f), glm::radians(90.f), glm::vec3(0.f, 0.f, 1.f), 'z');
-	renderPodium->Draw(ResourceManager::GetTexture("floorTexture"), pCamera, glm::vec3(-20.5f, 0.f, 0.f), glm::vec3(10.f, 1.f, 23.f), 0.0f, glm::vec3(), NULL);
-
 	// THIRD ROOM
 	renderer->Draw(ResourceManager::GetTexture("floorTexture"), pCamera, glm::vec3(50.0f, 0.f, 0.f), glm::vec3(), 0.f, glm::vec3(), NULL);
 
@@ -130,40 +133,20 @@ void Scene::RenderModels()
 	// Second Room - Cristina
 	
 	glm::mat4 modelM = glm::mat4();
-	modelM = glm::translate(modelM, glm::vec3(-20.0f, 0.5f, -1.0f));
+	modelM = glm::translate(modelM, glm::vec3(10.0f, 0.5f, -20.0f));
 	modelM = glm::scale(modelM, glm::vec3(0.1f, 0.1f, 0.1f));
-	modelM = glm::rotate(modelM, glm::radians(270.f), glm::vec3(1.f, 0.f, 0.f));
-	modelM = glm::rotate(modelM, glm::radians(90.f), glm::vec3(0.f, 0.f, 1.f));
+	modelM = glm::rotate(modelM, glm::radians(270.0f), glm::vec3(1.f, 0.f, 0.f));
+	modelM = glm::rotate(modelM, glm::radians(315.0f), glm::vec3(0.f, 0.f, 1.f));
 	glUniformMatrix4fv(glGetUniformLocation(modelShader.ID, "model"), 1, GL_FALSE, glm::value_ptr(modelM));
 	models->at("Wolf").Draw(modelShader);
 
 	modelM = glm::mat4();
-	modelM = glm::translate(modelM, glm::vec3(-22.0f, 0.0f, 13.0f));
-	modelM = glm::scale(modelM, glm::vec3(0.01f, 0.01f, 0.01f));
-	modelM = glm::rotate(modelM, glm::radians(270.f), glm::vec3(1.f, 0.f, 0.f));
+	modelM = glm::translate(modelM, glm::vec3(1.0f, 0.5f, -20.0f)); // Ajustează coordonatele de translație
+	modelM = glm::scale(modelM, glm::vec3(0.22f, 0.22f, 0.22f));
+	modelM = glm::rotate(modelM, glm::radians(270.0f), glm::vec3(1.f, 0.f, 0.f));
+	modelM = glm::rotate(modelM, glm::radians(-350.0f), glm::vec3(0.f, 0.f, 1.f));
 	glUniformMatrix4fv(glGetUniformLocation(modelShader.ID, "model"), 1, GL_FALSE, glm::value_ptr(modelM));
-	models->at("Tree").Draw(modelShader);
-
-	modelM = glm::mat4();
-	modelM = glm::translate(modelM, glm::vec3(-22.0f, 0.0f, -13.0f));
-	modelM = glm::scale(modelM, glm::vec3(0.01f, 0.01f, 0.01f));
-	modelM = glm::rotate(modelM, glm::radians(270.f), glm::vec3(1.f, 0.f, 0.f));
-	glUniformMatrix4fv(glGetUniformLocation(modelShader.ID, "model"), 1, GL_FALSE, glm::value_ptr(modelM));
-	models->at("Tree").Draw(modelShader);
-
-	modelM = glm::mat4();
-	modelM = glm::translate(modelM, glm::vec3(-22.0f, 0.01f, -5.0f));
-	modelM = glm::scale(modelM, glm::vec3(0.01f, 0.01f, 0.01f));
-	modelM = glm::rotate(modelM, glm::radians(270.f), glm::vec3(1.f, 0.f, 0.f));
-	glUniformMatrix4fv(glGetUniformLocation(modelShader.ID, "model"), 1, GL_FALSE, glm::value_ptr(modelM));
-	models->at("Tree").Draw(modelShader);
-
-	modelM = glm::mat4();
-	modelM = glm::translate(modelM, glm::vec3(-22.0f, 0.01f, 5.0f));
-	modelM = glm::scale(modelM, glm::vec3(0.01f, 0.01f, 0.01f));
-	modelM = glm::rotate(modelM, glm::radians(270.f), glm::vec3(1.f, 0.f, 0.f));
-	glUniformMatrix4fv(glGetUniformLocation(modelShader.ID, "model"), 1, GL_FALSE, glm::value_ptr(modelM));
-	models->at("Tree").Draw(modelShader);
+	models->at("Bear").Draw(modelShader);
 
 	// Third Room - Andrei
 
